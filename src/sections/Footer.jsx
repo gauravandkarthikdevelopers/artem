d319aad050ac7c969a97e086bf619654b88b8602
gauravkarthikeyan <gauravkarthik22@gmail.com>
2024-09-25 14:45:44 +0530

    second commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -165,7 +165,7 @@ const Footer = () => {
               target={"_blank"}
               rel="dofollow noreferrer"
             >
-              Gauravkarthik
+              Toby
             </a>
           </span>
         </Bottom>
